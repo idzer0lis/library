b37682ddfe1338a5fea8e843fdf2ec01d42725dc
zer0lis <idzer0lis@gmail.com>
2017-02-07 04:56:58 +0200

    heroku fix

--- djangovuejs/urls.py
+++ djangovuejs/urls.py
@@ -9,11 +9,7 @@ admin.autodiscover()
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -26,5 +22,8 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
 ]
 
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
