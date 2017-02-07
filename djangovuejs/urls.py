84767f26041a8eecf95ee07047f44e7765882b99
zer0lis <idzer0lis@gmail.com>
2017-02-07 04:58:10 +0200

    heroku fix

--- djangovuejs/urls.py
+++ djangovuejs/urls.py
@@ -6,11 +6,6 @@ from rest_framework import routers
 from library.views import index, getRecords, getRecord
 admin.autodiscover()
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'records', getRecords)
@@ -21,9 +16,3 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^api/', include(router.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
